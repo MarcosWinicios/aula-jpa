a71489f682f63a19982248b5dd4a7b78f3d19c1e
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2021-09-05 14:10:12 -0300

    created project

---
+++ src/main/java/com/marcoswinicios/aulajparepository/AulaJpareposioryApplication.java
@@ -0,0 +1,13 @@
+package com.marcoswinicios.aulajparepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaJpareposioryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaJpareposioryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcoswinicios/aulajparepository/AulaJpareposioryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcoswinicios.aulajparepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaJpareposioryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
